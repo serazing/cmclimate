a7d5077e6deab9c8bf38b8b8609ecc37c8aecd49
serazing <serazing@gmail.com>
2018-03-01 15:29:03 +0100

    Finalize the package

--- cmclimate/__init__.py
+++ cmclimate/__init__.py
@@ -1,9 +1,8 @@
 """
 cmclimate is a package containing the NCL colormaps to be used with matplotlib
 """
-
 from __future__ import absolute_import
 
 from . import cm
-
+__version__ = '0.1'
 __all__ = ['cm']
--- cmclimate/tests/test_cm.py
+++ cmclimate/tests/test_cm.py
@@ -8,9 +8,10 @@ import numpy as np
 
 def test_cmap_import():
     for name, cmap in vars(cm).items():
-        x = np.linspace(0, 10)
-        x2d, _ = np.meshgrid(x, x)
-        plt.figure()
-        plt.pcolor(x2d, cmap=cmap)
-        plt.title(name)
-        plt.close()
+        if isinstance(cmap, matplotlib.colors.ListedColormap):
+            x = np.linspace(0, 10)
+            x2d, _ = np.meshgrid(x, x)
+            plt.figure()
+            plt.pcolor(x2d, cmap=cmap)
+            plt.title(name)
+            plt.close()
