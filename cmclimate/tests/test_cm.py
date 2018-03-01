760cea9ea51ec0a092185acf0ac2a51d1bea1e26
serazing <serazing@gmail.com>
2018-03-01 14:52:02 +0100

    Fix some bugs for Travis CI

--- cmclimate/tests/test_cm.py
+++ cmclimate/tests/test_cm.py
@@ -1,13 +1,16 @@
 from __future__ import absolute_import, division, print_function
 from .. import cm
+import matplotlib
+# This 'agg' backend is required for Travis CI
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
 def test_cmap_import():
     for name, cmap in vars(cm).items():
-        print(name)
         x = np.linspace(0, 10)
         x2d, _ = np.meshgrid(x, x)
         plt.figure()
         plt.pcolor(x2d, cmap=cmap)
         plt.title(name)
+        plt.close()g=
